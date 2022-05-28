959e7e8b8107c73519c2ddb5b5504242714d8af6
SANO Koji <koji.sano@gmail.com>
2022-05-28 19:44:23 +0900

    default is JP layout

--- keyboards/ajisai74/keymaps/koji/keymap.c
+++ keyboards/ajisai74/keymaps/koji/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
   		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    LBRC,    RBRC,   BSLS,    KC_END,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
-		  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    SCLN,    QUOT,    KC_ENT, KC_ENT, KC_PGUP,
+		  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    SCLN,    QUOT,    KC_RCTL, KC_ENT, KC_PGUP,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
 		  SHIFT,   SHIFT,   KC_Z,    KC_X,    KC_C,    KC_V,        KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SHIFT,  KC_UP,    KC_PGDOWN,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
@@ -95,7 +95,7 @@ https://github.com/m47ch4n/qmk_firmware/blob/add-m47ch4ns-keymap/keyboards/ergod
 
 #define JP_LAYOUT true
 #define US_LAYOUT false
-bool LAYOUT_STATUS = US_LAYOUT;
+bool LAYOUT_STATUS = JP_LAYOUT;
 bool SHIFT_PRESSED = false;
 
 #define SEND_STRING_RESTORE(STR) (SHIFT_PRESSED ? SEND_STRING(STR SS_DOWN(X_LSHIFT)) : SEND_STRING(STR SS_UP(X_LSHIFT)))
